81e0367383819c563132c1f9eb1f5b27b81cb648
David Wilkinson <david.wilkinson1984@gmail.com>
2016-10-04 21:23:25 +0100

    allow a user to create a todo

---
+++ spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature 'UserCreatesTodo', type: :feature do
+  scenario 'successfully' do
+    visit(root_path)
+
+    click_on('Add a New Todo')
+    fill_in('Title', with: 'My First Todo')
+    click_on('Submit')
+
+    expect(page).to have_css('.todos li', text: 'My First Todo')
+  end
+end
