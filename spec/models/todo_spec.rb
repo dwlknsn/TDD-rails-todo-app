2aa1f87c47d0dc8061c0b94bd8429f41914f006d
David Wilkinson <david.wilkinson1984@gmail.com>
2016-10-06 10:55:18 +0100

    refactor to add Todo#complete!

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -2,14 +2,24 @@ require 'rails_helper'
 
 describe Todo, '#completed?' do
   it 'returns true if completed_at is set' do
-    todo = Todo.create!(completed_at: Time.current)
+    todo = Todo.new(completed_at: Time.current)
 
     expect(todo).to be_completed
   end
 
   it 'returns false if completed_at is nil' do
-    todo = Todo.create!(completed_at: nil)
+    todo = Todo.new(completed_at: nil)
 
     expect(todo).not_to be_completed
+  end
+end
+
+describe Todo, '#complete!' do
+  it 'update completed_at' do
+    todo = Todo.create!(completed_at: nil)
+
+    todo.complete!
+
+    expect(todo).to be_completed
   end
 end
